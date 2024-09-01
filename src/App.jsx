98d4ba8b57b482b09b976a369d3e390b211df2ee
DormantOne <131829942+DormantOne@users.noreply.github.com>
2024-09-01 19:41:56 -0400

    reformatted appearance to have two flanking panels - left for neural net and right for score.set up score table. need to calculate score data.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import { useState } from 'react';
-import './App.css';
-import FrameLimiter from './lib/FrameLimiter';
-import './Utils.css';
+import { useState } from "react";
+import "./App.css";
+import FrameLimiter from "./lib/FrameLimiter";
+import "./Utils.css";
 
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef } from "react";
 
 function App() {
   const frameLimiter = new FrameLimiter(60);
@@ -28,22 +28,94 @@ function App() {
   // Using acceleration would be relevant if we cared about how long a button is held
   // but this does not apply to flappy bird
 
-  const gameOverDisplayRef = useRef(null)
+  const gameOverDisplayRef = useRef(null);
 
-  const gameOverRef = useRef(true)
-
-  const scoreRef = useRef(0)
-
-  const scoreDisplayRef = useRef(null)
+  const gameOverRef = useRef(true);
 
   const birdStateRef = useRef({
     y: 240,
-    yVelocity: 0
+    yVelocity: 0,
   });
 
   const pipesStateRef = useRef([]);
 
-  const hasStartedRef = useRef(false)
+  const hasStartedRef = useRef(false);
+
+  // we need refs to track state of the metrics
+  // we need separate refs to html to change ui
+
+  const metricsStateRef = useRef({
+    score: 0,
+    bestScore: 0,
+    timeSurvived: 0,
+    bestTimeSurvived: 0,
+    ceilingHits: 0,
+    floorHits: 0,
+    barHits: 0,
+  });
+
+  const metricsTableScoreRef = useRef(null);
+  const metricsTableBestScoreRef = useRef(null);
+  const metricsTableTimeSurvivedRef = useRef(null);
+  const metricsTableBestTimeSurvivedRef = useRef(null);
+  const metricsTableCeilingHitsRef = useRef(null);
+  const metricsTableFloorHitsRef = useRef(null);
+  const metricsTableBarHitsRef = useRef(null);
+
+  function updateMetricsTableInUI() {
+    const score = metricsStateRef.current.score;
+    const bestScore = metricsStateRef.current.bestScore;
+    const timeSurvived = metricsStateRef.current.timeSurvived;
+    const bestTimeSurvived = metricsStateRef.current.bestTimeSurvived;
+    const ceilingHits = metricsStateRef.current.ceilingHits;
+    const floorHits = metricsStateRef.current.floorHits;
+    const barHits = metricsStateRef.current.barHits;
+
+    metricsTableScoreRef.current.textContent = score.toString();
+    metricsTableBestScoreRef.current.textContent = bestScore.toString();
+    metricsTableTimeSurvivedRef.current.textContent = timeSurvived.toFixed(3);
+    metricsTableBestTimeSurvivedRef.current.textContent =
+      bestTimeSurvived.toFixed(3);
+    metricsTableCeilingHitsRef.current.textContent = ceilingHits.toString();
+    metricsTableFloorHitsRef.current.textContent = floorHits.toString();
+    metricsTableBarHitsRef.current.textContent = barHits.toString();
+  }
+
+  function checkMetricName(metricName) {
+    if (!Object.keys(metricsStateRef.current).includes(metricName)) {
+      throw new Error(`
+Invalid metric name: '${metricName}'. Check for typos in your code.
+Available Metrics:
+${Object.keys(metricsStateRef.current).join(", ")}
+        `);
+    }
+  }
+
+  /**
+   *
+   * @param {string} metricName
+   *
+   * @returns {number}
+   */
+  function getMetric(metricName) {
+    checkMetricName(metricName);
+    return metricsStateRef.current[metricName];
+  }
+
+  /**
+   *
+   * @param {string} metricName
+   * @param {number} value
+   */
+  function setMetricAndUpdateUI(metricName, value) {
+    checkMetricName(metricName);
+    metricsStateRef.current[metricName] = value;
+    updateMetricsTableInUI();
+  }
+
+  function incrementScore() {
+    setMetricAndUpdateUI("score", getMetric("score") + 1);
+  }
 
   function clearPipes() {
     pipesStateRef.current = [];
@@ -56,48 +128,45 @@ function App() {
       pipesStateRef.current.push({
         x: pipeX,
         y: pipeY,
-        scoreable: true
-      })
+        scoreable: true,
+      });
     } else {
-
       let rightmostPipe = {
         x: 0,
-        y: 0
-      }
+        y: 0,
+      };
 
       for (const pipe of pipesStateRef.current) {
         if (pipe.x >= rightmostPipe.x) {
-          rightmostPipe = pipe
+          rightmostPipe = pipe;
         }
       }
 
       existingPipe.x = rightmostPipe.x + PIPE_SPACING;
-      existingPipe.y = pipeY
-      existingPipe.scoreable = true
-
+      existingPipe.y = pipeY;
+      existingPipe.scoreable = true;
     }
-
   }
 
   function resetGame() {
-    hasStartedRef.current = true
-    gameOverDisplayRef.current.style.display = "none"
-    gameOverDisplayRef.current.textContent = "Game Over"
-    gameOverDisplayRef.current.style.color = "red"
+    hasStartedRef.current = true;
+    gameOverDisplayRef.current.style.display = "none";
+    gameOverDisplayRef.current.textContent = "Game Over";
+    gameOverDisplayRef.current.style.color = "red";
     birdStateRef.current.y = 240;
     birdStateRef.current.yVelocity = 0;
-    gameOverDisplayRef.current.style.display = "none"
-    gameOverRef.current = false
-    scoreRef.current = 0
-    clearPipes()
+    gameOverDisplayRef.current.style.display = "none";
+    gameOverRef.current = false;
+    setMetricAndUpdateUI("score", 0);
+    clearPipes();
     for (let i = 0; i < numBufferedPipes; i++) {
-      spawnOrReusePipe(i * PIPE_SPACING, null)
+      spawnOrReusePipe(i * PIPE_SPACING, null);
     }
   }
 
   function setGameOver() {
-    gameOverRef.current = true
-    gameOverDisplayRef.current.style.display = "flex"
+    gameOverRef.current = true;
+    gameOverDisplayRef.current.style.display = "flex";
   }
 
   function gameLoop() {
@@ -108,19 +177,16 @@ function App() {
       return;
     }
 
-    const ctx = canvas.getContext('2d');
-    const deltaTime = frameLimiter.getDeltaTime()
+    const ctx = canvas.getContext("2d");
+    const deltaTime = frameLimiter.getDeltaTime();
     if (deltaTime === undefined) {
       window.requestAnimationFrame(gameLoop);
-      return
+      return;
     }
 
     if (fpsDisplayRef.current) {
-      fpsDisplayRef.current.textContent = (1.0 / deltaTime).toFixed(3)
+      fpsDisplayRef.current.textContent = (1.0 / deltaTime).toFixed(3);
     }
-
-
-
 
     // Clear canvas
     ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -128,25 +194,38 @@ function App() {
     // Draw bird
     // ctx.beginPath();
     // ctx.arc(BIRD_X, birdStateRef.current.y, BIRD_RADIUS, 0, 2 * Math.PI);
-    ctx.fillStyle = 'red';  // You can choose the fill color
+    ctx.fillStyle = "red"; // You can choose the fill color
     // ctx.fill();
     // ctx.closePath();
 
-    ctx.fillRect(BIRD_X - BIRD_RADIUS, birdStateRef.current.y - BIRD_RADIUS, 2 * BIRD_RADIUS, 2 * BIRD_RADIUS)
+    ctx.fillRect(
+      BIRD_X - BIRD_RADIUS,
+      birdStateRef.current.y - BIRD_RADIUS,
+      2 * BIRD_RADIUS,
+      2 * BIRD_RADIUS
+    );
 
     // Draw Pipes
     for (const pipe of pipesStateRef.current) {
       // The pipe y is the centerpoit of teh gap (thin air)
       // the pipe x is te center of the pipe on the x axis (halfway through the thickness)
       // Draw the top pipe (above the gap)
-      ctx.fillStyle = 'green';
-      ctx.fillRect(pipe.x - PIPE_WIDTH / 2, 0, PIPE_WIDTH, pipe.y - PIPE_WINDOW_SIZE / 2);
+      ctx.fillStyle = "green";
+      ctx.fillRect(
+        pipe.x - PIPE_WIDTH / 2,
+        0,
+        PIPE_WIDTH,
+        pipe.y - PIPE_WINDOW_SIZE / 2
+      );
 
       // Draw the bottom pipe (below the gap)
-      ctx.fillRect(pipe.x - PIPE_WIDTH / 2, pipe.y + PIPE_WINDOW_SIZE / 2, PIPE_WIDTH, canvas.height - pipe.y - PIPE_WINDOW_SIZE / 2);
-
+      ctx.fillRect(
+        pipe.x - PIPE_WIDTH / 2,
+        pipe.y + PIPE_WINDOW_SIZE / 2,
+        PIPE_WIDTH,
+        canvas.height - pipe.y - PIPE_WINDOW_SIZE / 2
+      );
     }
-
 
     //collision check
     if (birdStateRef.current.y >= 480) {
@@ -161,18 +240,20 @@ function App() {
     for (const pipe of pipesStateRef.current) {
       // first, check if pipe has any chance to be relevant
       if (
-        (pipe.x + PIPE_WIDTH / 2 >= BIRD_X - BIRD_RADIUS) &&
-        (pipe.x - PIPE_WIDTH / 2 <= BIRD_X + BIRD_RADIUS)
+        pipe.x + PIPE_WIDTH / 2 >= BIRD_X - BIRD_RADIUS &&
+        pipe.x - PIPE_WIDTH / 2 <= BIRD_X + BIRD_RADIUS
       ) {
         // then check if bird is outside window
-        if (!(
-          ((birdStateRef.current.y - BIRD_RADIUS) >= (pipe.y - PIPE_WINDOW_SIZE / 2)) &&
-          (
-            (birdStateRef.current.y + BIRD_RADIUS) <= (pipe.y + PIPE_WINDOW_SIZE / 2)
+        if (
+          !(
+            birdStateRef.current.y - BIRD_RADIUS >=
+              pipe.y - PIPE_WINDOW_SIZE / 2 &&
+            birdStateRef.current.y + BIRD_RADIUS <=
+              pipe.y + PIPE_WINDOW_SIZE / 2
           )
-        )) {
-          setGameOver()
-          break
+        ) {
+          setGameOver();
+          break;
         }
       }
     }
@@ -184,32 +265,27 @@ function App() {
       birdStateRef.current.y += birdStateRef.current.yVelocity * deltaTime;
       // move pipes
       for (const pipe of pipesStateRef.current) {
-        pipe.x -= PIPE_VELOCITY * deltaTime
+        pipe.x -= PIPE_VELOCITY * deltaTime;
         // 2pixel buffer just in case
         if (pipe.x < BIRD_X - BIRD_RADIUS - PIPE_WIDTH / 2) {
           if (pipe.scoreable) {
-            scoreRef.current += 1
-            scoreDisplayRef.current.textContent = scoreRef.current.toString()
+            incrementScore();
           }
-          pipe.scoreable = false
+          pipe.scoreable = false;
         }
         if (pipe.x < -PIPE_WIDTH / 2 - 2) {
-
-          spawnOrReusePipe(PIPE_SPACING, pipe)
+          spawnOrReusePipe(PIPE_SPACING, pipe);
         }
-
       }
     }
 
     window.requestAnimationFrame(gameLoop);
   }
 
-
-
   function handleKeyPress(e) {
-    if (e.code === 'Space') {
+    if (e.code === "Space") {
       if (gameOverRef.current) {
-        resetGame()
+        resetGame();
       } else {
         birdStateRef.current.yVelocity -= FLAP_VELOCITY;
       }
@@ -217,9 +293,9 @@ function App() {
   }
 
   useEffect(() => {
-    window.addEventListener('keydown', handleKeyPress);
+    window.addEventListener("keydown", handleKeyPress);
     return () => {
-      window.removeEventListener('keydown', handleKeyPress);
+      window.removeEventListener("keydown", handleKeyPress);
     };
   }, []);
 
@@ -230,62 +306,144 @@ function App() {
   }, [canvasRef.current]);
 
   return (
-    <div className='vstack' style={{
-      width: '100%',
-      height: "100%"
-    }}>
-      <div style={{
-        flex: 1
-      }}></div>
-      <div style={{
-        position: "relative",
-        flex: 0
-      }}>
-        <canvas ref={canvasRef} style={{
-          border: '2px solid black',
-          width: '640px',
-          height: '480px'
-        }} width='640' height='480'></canvas>
-        <div style={{
-          position: "absolute",
-          top: 0,
-          left: 0,
-          color: "black",
-          fontSize: "16px"
-        }} ref={fpsDisplayRef}></div>
-        <div style={{
-          position: "absolute",
-          top: "8px",
-          right: "8px",
-          color: "blue",
-          fontSize: "32px",
-          lineHeight: "32px",
-          margin: 0,
-          padding: 0
-        }} ref={scoreDisplayRef}>0</div>
-        <div style={{
-          display: "flex",
-          position: "absolute",
-          top: 0,
-          left: 0,
-          right: 0,
-          bottom: 0,
-          flexDirection: "column",
-          alignItems: "center",
-          justifyContent: "center"
-        }} >
-          <h1 style={{
-            fontSize: "64px",
-            fontWeight: "bold",
-            color: "blue"
-          }} ref={gameOverDisplayRef}>Press Space to Play</h1>
+    <div
+      className="hstack"
+      style={{
+        width: "100%",
+        height: "100%",
+        padding: "4px",
+        gap: "4px",
+      }}
+    >
+      <div
+        className="vispanel vispanel-neuralnet"
+        style={{
+          flex: 1,
+        }}
+      ></div>
+
+      <div
+        className="vstack"
+        style={{
+          width: "100%",
+          height: "100%",
+          flex: 0,
+        }}
+      >
+        <div
+          style={{
+            flex: 1,
+          }}
+        ></div>
+        <div
+          style={{
+            position: "relative",
+            flex: 0,
+          }}
+        >
+          <canvas
+            ref={canvasRef}
+            style={{
+              border: "2px solid black",
+              width: "640px",
+              height: "480px",
+            }}
+            width="640"
+            height="480"
+          ></canvas>
+          <div
+            style={{
+              position: "absolute",
+              top: 0,
+              left: 0,
+              color: "black",
+              fontSize: "16px",
+            }}
+            ref={fpsDisplayRef}
+          ></div>
+
+          <div
+            style={{
+              display: "flex",
+              position: "absolute",
+              top: 0,
+              left: 0,
+              right: 0,
+              bottom: 0,
+              flexDirection: "column",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <h1
+              style={{
+                fontSize: "64px",
+                fontWeight: "bold",
+                color: "blue",
+              }}
+              ref={gameOverDisplayRef}
+            >
+              Press Space to Play
+            </h1>
+          </div>
         </div>
+        <div
+          style={{
+            flex: 1,
+          }}
+        ></div>
       </div>
-      <div style={{
-        flex: 1
-      }}></div>
+      <div
+        className="vispanel vispanel-metrics vstack"
+        style={{
+          flex: 1,
+        }}
+      >
+        {/* 
+        will put table here - the table will have score, time elapsed, 
+        distance to nearest pipe, survival time, best time survived
+        */}
+        <table>
+          <thead>
+            <tr>
+              <th>Metric Name</th>
+              <th>Metric Value</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Score (# pipes)</td>
+              <td ref={metricsTableScoreRef}>0</td>
+            </tr>
+            <tr>
+              <td>Best Score (# pipes)</td>
+              <td ref={metricsTableBestScoreRef}>0</td>
+            </tr>
+            <tr>
+              <td>Time Survived (s)</td>
+              <td ref={metricsTableTimeSurvivedRef}>0</td>
+            </tr>
+            <tr>
+              <td>Best Time Survived (s)</td>
+              <td ref={metricsTableBestTimeSurvivedRef}>0</td>
+            </tr>
+            <tr>
+              <td>Ceiling Hits</td>
+              <td ref={metricsTableCeilingHitsRef}>0</td>
+            </tr>
+            <tr>
+              <td>Floor Hits</td>
+              <td ref={metricsTableFloorHitsRef}>0</td>
+            </tr>
+            <tr>
+              <td>Bar Hits</td>
+              <td ref={metricsTableBarHitsRef}>0</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
